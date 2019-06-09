50d0848c955d921cbfa6983957ac1dfde6abf3de
Santeri Saariokari <santeri.saariokari@tuni.fi>
2019-06-09 13:54:25 +0300

    unicafe step6

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom'
 
 const Headers = props => <h1>{props.text}</h1>
 
-const Display = props => <div>{props.text} {props.value}</div>
+const Display = props => {
+  return(
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  )
+}
 
 const Presentage = (props) => {
   return(
-    <div>
-        {props.text} {props.good * 100 / (props.good + props.neutral + props.bad)} %
-    </div>
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.good * 100 / (props.good + props.neutral + props.bad)} % </td>
+    </tr>
   )
 }
 
@@ -23,14 +31,18 @@ const Statistics = (props) => {
   if (props.good > 0 || props.bad > 0 || props.neutral > 0){
     return(
       <div>
-          <Headers text={'statistics'} />
-          <Display text={'good'} value={props.good} />
-          <Display text={'neutral'} value={props.neutral} />
-          <Display text={'bad'} value={props.bad} />
-          <Display text={'all'} value={props.good + props.neutral + props.bad} />
-          <Display text={'average'} value={
-              (props.good - props.bad) / (props.good + props.neutral + props.bad)} />
-          <Presentage text={'positive'} good={props.good} neutral={props.neutral} bad={props.bad} />
+        <Headers text={'statistics'} />
+        <table>
+          <tbody>
+            <Display text={'good'} value={props.good} />
+            <Display text={'neutral'} value={props.neutral} />
+            <Display text={'bad'} value={props.bad} />
+            <Display text={'all'} value={props.good + props.neutral + props.bad} />
+            <Display text={'average'} value={
+                (props.good - props.bad) / (props.good + props.neutral + props.bad)} />
+            <Presentage text={'positive'} good={props.good} neutral={props.neutral} bad={props.bad} />
+          </tbody>
+        </table>
       </div>
     )
  }
